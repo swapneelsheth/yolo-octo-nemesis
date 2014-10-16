e0ccf5460c434bfc6f902052ae69c33550f4cf99
Swapneel Sheth <swapneel@cis.upenn.edu>
2014-10-16 17:41:48 -0400

    initial commit for in-class

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotify::Application.config.secret_token = '368a210a0b0390f1e85e9241dbf455cba809c1619473f6528e324e1f7d38ab2c450b10d7166472262cc0c555aef4761dd2152d84cc3e08aa4ffc71241f75a3fc'
